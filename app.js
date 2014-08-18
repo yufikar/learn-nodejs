813f7a72647e43ab89815c5d896821de796abc33
yufikar <yufikar@gmail.com>
2014-08-18 13:37:28 +0000

    lagi

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var http        = require('http');
 // hello world via browser
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("Hello World");
+  res.end("Hello dunia");
 }).listen(process.env.PORT);
