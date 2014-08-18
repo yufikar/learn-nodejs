9dadf3d9121f508e966e6674a72e5b2e8b7901a5
yufikar <yufikar@gmail.com>
2014-08-18 14:01:18 +0000

    tambah label

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var http        = require('http');
 
+var headerContent = {
+  'Content-Type' : 'text/plain',
+  'haha' : 'test'
+};
+
+// console.log(headerContent['Content-Type']);
+
+function onHttpRequest(a, b) {
+    b.writeHead(200, headerContent);
+    b.write("Test tulis ke browser\");
+    b.end("Hello dunia");
+}
+
+var server = http.createServer(onHttpRequest);
+server.listen(process.env.PORT);
+
 // hello world via browser
+/*
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end("Hello dunia");
 }).listen(process.env.PORT);
+*/
